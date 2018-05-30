7766fb04d9781a3009af42b6b4ea4e6df9109d92
Abhinav Kansal <abhinavkansal@gmail.com>
2018-05-30 08:27:29 +0530

    added

--- JenkinsFile-build.groovy
+++ JenkinsFile-build.groovy
@@ -3,17 +3,17 @@ node(docker){
 
     stage "Build Room Services"
         echo "clean and build room services"
-        sh "./gradlew clean build"
-        echo "room services build completed ${env.BUILD_ID}"
+        sh "gradle clean build"
+        //echo "room services build completed ${env.BUILD_ID}"
 
-    stage "Building ${container_name} Container for Core-Services"
-
-        echo "Building ${container_name} with docker.build(${registry_url}"
-        // add more tests
-        sh "gradle buildDocker"
-        // Set up the container to build
-
-        echo "docker image successfully created"
+//    stage "Building ${container_name} Container for Core-Services"
+//
+//        echo "Building ${container_name} with docker.build(${registry_url}"
+//        // add more tests
+//        sh "gradle buildDocker"
+//        // Set up the container to build
+//
+//        echo "docker image successfully created"
 
 //Tag the image built from previuos stage
 }
